ef642eede07a70b623b6635d7ff1cf74ce5f59cf
im-a-kookie <katherinesprod@gmail.com>
2024-12-22 16:33:30 +1030

    Building out program initialization

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,15 @@
 ﻿using Backend.API;
+using Backend.Config;
 using Cookie.Connections.API;
+using Cookie.Connections.API.Logins;
+using Cookie.ContentLibrary;
+using Cookie.Logging;
 using Cookie.Serializers;
 using Cookie.Serializers.Bytewise;
 using Cookie.Serializing;
+using System;
 using System.Diagnostics;
+using System.Net;
 
 namespace Backend.ServerLibrary
 {
@@ -12,45 +18,58 @@ namespace Backend.ServerLibrary
         public static JsonSerialization localSerializer = new();
         public static JsonSerialization remoteSerializer = new();
 
+        
+
+
+
         public static void Main(string[] args)
         {
+            Configurator config = new Configurator();
+
 
             var b = new Controller<Program>(new Program());
 
+            // setup a provider
+            Library library = new("S:/");
+            LibraryProvider provider = new LibraryProvider(library);
+            b.SetProvider(provider.Provider);
+            var loginManager = new SimpleLoginManager<Program>(b);
+            b.SetLoginManager(loginManager);
 
-            var l = new Login();
+            // Set up the API
+            b.Discover<Login>();
+            b.Discover<Files>()!.Provider = provider;
 
-            b.Discover<Login>(l);
-
-            var searcher = new Searcher("E:/");
-            var lib = searcher.Enumerate(4);
-            lib.CompressPaths();
-            Directory.CreateDirectory("Testing");
-            Stopwatch s = Stopwatch.StartNew();
-
-            using var ms = new MemoryStream();
-            Byter.ToBytes(ms, ((IDictable)lib).MakeDictionary());
-            File.WriteAllBytes("Total.txt", ms.ToArray());
+            // And start the server
+            b.ProvideStandardWebserver();
+            b.StartHttp(config.Port);
 
 
-            foreach (var item in lib.FoundSeries)
+            var cookie = new System.Net.Cookie("LoginToken", loginManager.NameUsers.First().Value.GenerateToken(TimeSpan.FromDays(1)));
+
+            // Create a CookieContainer and add a cookie
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(new Uri($"http://127.0.0.1:{config.Port}/"), cookie);
+
+            // Create HttpClientHandler with the container
+            var handler = new HttpClientHandler
             {
-                try
+                CookieContainer = cookieContainer
+            };
+            using HttpClient http = new(handler);
+
+            while (true)
+            {
+                var str = Console.ReadLine();
+
+                if (str == "refresh")
                 {
-                    File.Delete($"Testing/{item.Value.Name}.txt");
-                    using var file = File.OpenWrite($"Testing/{item.Value.Name}.txt");
-                    Byter.ToBytes(file, ((IDictable)item.Value).MakeDictionary());
+                    var task = Task.Run(() => http.GetAsync($"http://127.0.0.1:{config.Port}/content/refresh"));
+                    task.Wait();
+                    var response = task.Result;
+                    Logger.Info($"Response: {response.StatusCode}");
                 }
-                catch { }
-
             }
-            Console.WriteLine("Time: " + s.Elapsed.TotalMilliseconds);
-
-
-            Console.WriteLine("Done!");
-
-
-
 
 
         }
