3cb35495dafa4289b32ed5022d2964299beff8a2
im-a-kookie <katherinesprod@gmail.com>
2024-12-27 13:20:32 +1030

    Configuration switched from embedded to local jsons

--- Backend/Config/Configurator.cs
+++ Backend/Config/Configurator.cs
@@ -1,44 +1,61 @@
-﻿using Cookie.Cryptography;
+﻿using Cookie.Connections.API.Logins;
+using Cookie.Cryptography;
 using Cookie.Serializers;
 using Cookie.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace Backend.Config
+namespace Server.Config
 {
-    internal class Configurator
+    public class Configurator
     {
+        public Task LoadingTask;
 
         public int Port { get; private set; } = 12345;
+
+        public LoginData DefaultUser = new();
 
         /// <summary>
         /// Creates and loads configuration settings for the application
         /// </summary>
         public Configurator()
         {
-            RunConfigurations();
+            LoadingTask = Task.Run(async () =>
+            {
+                await RunConfigurations();
+            });
         }
 
         /// <summary>
         /// Runs all application configurations
         /// </summary>
-        private void RunConfigurations()
+        private Task RunConfigurations()
         {
-            LoadServerSettings();
-            RunCryptoConfiguration();
+            return Task.WhenAll(
+                LoadServerSettings(),
+                RunCryptoConfiguration(),
+                LoadDefaultUserSettings()
+            );
         }
 
         /// <summary>
         /// Loads the settings for the server
         /// </summary>
-        private void LoadServerSettings()
+        private async Task LoadServerSettings()
         {
-            var data = ResourceTool.GetResource("Backend.Config.ServerSettings.json");
-            if(data == null) return;
+            string? data = null;
+            try
+            {
+                data = await File.ReadAllTextAsync("/Config/server.json");
+            }
+            catch
+            { }
+            if (data == null) return;
             
             var dict = JsonSerializer.Deserialize<Dictionary<string, object>>(data!);
             if (dict == null) return;
@@ -47,19 +64,44 @@ namespace Backend.Config
             {
                 Port = i;
             }
+        }
 
+        /// <summary>
+        /// Loads the settings for the server
+        /// </summary>
+        private async Task LoadDefaultUserSettings()
+        {
+            string? data = null;
+            try
+            {
+                data = await File.ReadAllTextAsync("/Config/login.json");
+            }
+            catch
+            { }
+            if (data == null) return;
+
+            var DefaultUser = JsonSerializer.Deserialize<LoginData>(data!);
+         
         }
 
 
         /// <summary>
         /// Setup default application cryptography key stuff
         /// </summary>
-        private void RunCryptoConfiguration()
+        private static async Task RunCryptoConfiguration()
         {
             try
             {
+
+                string? data = null;
+                try
+                {
+                    data = await File.ReadAllTextAsync("/Config/crypto.json");
+                }catch
+                { }
+
                 // load the key out of the encryption configuration json
-                var data = ResourceTool.GetResource("Backend.Config.Encryption.json") ?? "{\"application_aes_key\":\"Default Aes Application Key!\"}";
+                data ??= "{\"application_aes_key\":\"Default Aes Application Key!\"}";
                 var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(data!);
                 string? key = "";
                 if(dict == null || !dict.TryGetValue("application_aes_key", out key))
